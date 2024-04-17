405770b068a56a8de60c2379d565f922a4347740
Sajal Sharma <79101773+SajalSharma4@users.noreply.github.com>
2024-04-17 21:54:10 +0530

    Add files via upload

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import Card from './Card';
+import FormField from './FormField';
+import Loader from './Loader';
+import LoginField from './LoginField';
+import SignUpField from './SignUpField';
+
+export {
+  Card,
+  FormField,
+  Loader,
+  LoginField,
+  SignUpField
+};
